6e2db1263afff0202573657194fba0df5038ad9c
Juan Pablo Claros Romero <alu0100972603@ull.edu.es>
2017-11-07 19:19:05 +0000

    Existe nodo con dato, siguiente y previo(corregido)

--- lib/P06/list.rb
+++ lib/P06/list.rb
@@ -2,4 +2,69 @@ Nodo = Struct.new(:value, :next, :prev)
 
 class List
 
+   attr_accessor :head, :tail
+   
+    def initialize
+       @head = nil
+       @tail = nil
+    end
+    
+    def push_head (nodo)
+        if @head == nil
+            @head = Nodo.new(nodo,nil,nil)
+            @tail = @head
+        else
+            @head.next = Nodo.new(nodo,nil,@head)
+            @head = @head.next
+        end
+    end
+
+    def get_value(value)
+        if @head == nil   #Comprobamos si la lista no esta vacía
+            raise RuntimeError, "Lista vacía, no se puede extraer nodo"
+        else
+		    i = @tail
+		    @valor=nil
+		    while i != nil
+		        if i.value == value
+		            @valor = i.value
+		        end
+		        i = i.next
+		    end
+        end
+        @valor
+    end
+    
+    def get_next(value)
+        if @head == nil   #Comprobamos si la lista no esta vacía
+            raise RuntimeError, "Lista vacía, no se puede extraer nodo"
+        else
+		    i = @tail
+		    @valor = nil
+		    while i != nil
+		        if i.value == value
+		            @valor = i.next
+		        end
+		        i = i.next
+		    end
+        end
+        @valor
+    end
+    
+    def get_prev(value)
+        if @head == nil   #Comprobamos si la lista no esta vacía
+            raise RuntimeError, "Lista vacía, no se puede extraer nodo"
+        else
+		    i = @tail
+		    @valor = nil
+		    while i != nil
+		        if i.value == value
+		            @valor = i.prev
+		        end
+		        i = i.next
+		    end
+        end
+        @valor
+    end
+
 end
