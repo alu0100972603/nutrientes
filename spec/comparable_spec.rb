bf1aa39216939f1b97ebe11916e61b37c99ca33f
Juan Pablo Claros Romero <alu0100972603@ull.edu.es>
2017-11-13 19:22:40 +0000

    Existe un metodo que comprueba la igualdad de dos alimentos

--- spec/comparable_spec.rb
+++ spec/comparable_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Comparable" do
 		@huevo=Alimento_concreto.new("Huevos, lacteos y helados","Huevo",14.1,0.0,19.5)
 		@leche=Alimento_concreto.new("Huevos, lacteos y helados","Leche_vaca",3.3,4.8,3.2)
 		@yogurt=Alimento_concreto.new("Huevos, lacteos y helados","Yogurt",3.8,4.9,3.8)
+		@leche2=Alimento_concreto.new("Huevo, lacteos y helados","Leche_vaca2",3.3,4.8,3.2)
 	end
 
 	it "Comprobando que calorias de leche de vaca es menor que calorias de huevo" do
@@ -14,6 +15,10 @@ RSpec.describe "Comparable" do
 
 	it "Comprobando que calorias de yogurt es mayor que calorias de leche" do
 		expect(@yogurt>@leche).to be(true)
+	end
+
+	it "Comprobando que calorias de leche de vaca es igual a calorias de leche de vaca2" do
+		expect(@leche==@leche2).to be(true)
 	end
 
 end
